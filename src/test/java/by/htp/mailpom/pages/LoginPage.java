3204ae8c8194e1c6a212f14983018002b675724f
Kris <celly@inbox.ru>
2018-07-25 20:21:24 +0300

    DriverSingleton is added

--- src/test/java/by/htp/mailpom/pages/LoginPage.java
+++ src/test/java/by/htp/mailpom/pages/LoginPage.java
@@ -1,8 +1,16 @@
 package by.htp.mailpom.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class LoginPage extends AbstractPage{
+	
+	private static final String BASE_URL = "https://mail.ru/";
+	
+	private WebElement inputLogin;
+	private WebElement inputPass;
+	private WebElement buttonSubmit;
+	private WebElement linkLoggedInUser;
 
 	public LoginPage(WebDriver driver) {
 		super(driver);
