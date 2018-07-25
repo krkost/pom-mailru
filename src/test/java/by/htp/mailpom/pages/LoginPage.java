4ee509b541d0cfae3dd30558051639610e024064
Kris <celly@inbox.ru>
2018-07-25 20:37:36 +0300

    LoginPage is added

--- src/test/java/by/htp/mailpom/pages/LoginPage.java
+++ src/test/java/by/htp/mailpom/pages/LoginPage.java
@@ -1,12 +1,13 @@
 package by.htp.mailpom.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class LoginPage extends AbstractPage{
-	
+public class LoginPage extends AbstractPage {
+
 	private static final String BASE_URL = "https://mail.ru/";
-	
+
 	private WebElement inputLogin;
 	private WebElement inputPass;
 	private WebElement buttonSubmit;
@@ -18,6 +19,13 @@ public class LoginPage extends AbstractPage{
 
 	@Override
 	public void openPage() {
+		driver.get(BASE_URL);
+	}
+
+	public void login(String username, String password) {
+		inputLogin.findElement(By.xpath("//div[@id='mailbox:loginContainer']/child::*")).sendKeys(username);
+		inputPass.findElement(By.xpath("//div[@class='mailbox__input__container']/child::*[1]")).sendKeys(password);
+		buttonSubmit.findElement(By.xpath("//input[@class='o-control']")).click();
 	}
 
 }
