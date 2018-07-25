f06167cf0cda0b1c90d87b8e9afe1c684978660a
Kris <celly@inbox.ru>
2018-07-25 20:53:21 +0300

    MailAutomationTest is added

---
+++ src/test/java/by/htp/mailpom/MailAutomationTest.java
@@ -0,0 +1,29 @@
+package by.htp.mailpom;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import by.htp.mailpom.steps.Steps;
+
+public class MailAutomationTest {
+	
+	private Steps steps;
+	private final String USERNAME = "tathtp";
+	private final String PASSWORD = "Klopik123";
+	
+	@BeforeMethod
+	public void setUp()
+	{
+		steps = new Steps();
+		steps.initBrowser();
+	}
+	
+	@Test
+	public void oneCanLoginGithub()
+	{
+		steps.loginMail(USERNAME, PASSWORD);
+		Assert.assertNotNull(steps);
+	}
+
+}
